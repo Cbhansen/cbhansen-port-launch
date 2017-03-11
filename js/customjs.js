d8aec287ec24b724a5fc1c4de967393246b111a7
Chris <chansenwebdevanddesigns@gmail.comgit config --global user.email chansenwebdevanddesigns@gmail.com>
2017-03-11 01:34:06 -0800

    jquery added to footer

--- js/customjs.js
+++ js/customjs.js
@@ -1,5 +1,18 @@
-/* FADE IN */
+/**********************************
+FADE IN ON LOAD
+***********************************/
 
 $(function() {
     $("#wrapper").fadeIn(1000);
+});
+
+
+/**********************************
+Footer- Social links hover effect
+***********************************/
+
+$(function() {
+    $('footer .fa').mouseenter(function() {
+        $(this).effect('bounce',1000);
+    })
 });
